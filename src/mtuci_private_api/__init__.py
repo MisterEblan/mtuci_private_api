215dbcc0313a51dd378c78bcd358fec998957517
MisterEblan <boy4rov.da@gmail.com>
2025-11-21 20:35:12 +0300

    feat(init): added classes to init

--- src/mtuci_private_api/__init__.py
+++ src/mtuci_private_api/__init__.py
@@ -1,1 +1,31 @@
 """Библиотека для работы с личным кабинетом МТУСИ"""
+
+from .mtuci import Mtuci
+from .models import (
+        User,
+        Attendance,
+        Schedule,
+        Lesson,
+        LessonType
+)
+from .errors import (
+    AuthError,
+    GetAttendanceError,
+    GetScheduleError,
+    GetUserInfoError,
+    ParseError
+)
+
+__all__ = [
+    "Mtuci",
+    "User",
+    "Attendance",
+    "Schedule",
+    "Lesson",
+    "LessonType",
+    "AuthError",
+    "GetAttendanceError",
+    "GetScheduleError",
+    "GetUserInfoError",
+    "ParseError"
+]
